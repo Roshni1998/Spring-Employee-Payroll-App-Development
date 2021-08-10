2c55551963f7356e01339777758bbc566877580c
Roshni1998 <roshnimali847@gmail.com>
2021-08-10 17:33:25 +0530

    Added: UC-2 Employee Payroll Application

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,6 +3,10 @@ package com.bridgelabz.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @author ROSHNI
+ * EmployeePayrollApplication
+ * */
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
