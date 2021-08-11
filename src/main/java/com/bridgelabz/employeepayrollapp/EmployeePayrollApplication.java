b75e0f5da1efa1ec5dfaccce76f011c1c9f370dc
Roshni1998 <roshnimali847@gmail.com>
2021-08-11 19:36:27 +0530

    Added: UC-2 Use Lombok Library for Logging

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.employeepayrollapp;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/*
+ * @author ROSHNI
+ * EmployeePayrollApplication
+ * */
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB use is", context.getEnvironment().getProperty("spring.datasource.username"));
+
+	}
+
+}
