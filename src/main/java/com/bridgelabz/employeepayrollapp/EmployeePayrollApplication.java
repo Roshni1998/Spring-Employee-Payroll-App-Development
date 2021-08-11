815284acfbed25570fe6f015ceccb33541b1891d
Roshni1998 <roshnimali847@gmail.com>
2021-08-11 21:20:00 +0530

    Added: UC-3 Testing the Environments

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -18,8 +18,5 @@ public class EmployeePayrollApplication {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment",
 				context.getEnvironment().getProperty("environment"));
-		log.info("Employee Payroll DB use is", context.getEnvironment().getProperty("spring.datasource.username"));
-
 	}
-
 }
