5fd240a61ae6878b1cb38ada3a3fe2504849aad5
Roshni1998 <roshnimali847@gmail.com>
2021-08-13 00:02:13 +0530

    Added: UC-3 Data Validation Regex Pattern

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name Invalid")
     public String name;
+
+    @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
