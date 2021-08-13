1d9f9c745f121ec66bc3f64aeaa0df9aac1ea3e2
Roshni1998 <roshnimali847@gmail.com>
2021-08-13 19:44:57 +0530

    Added: UC-1 Add remaining properties to the Payroll DTO and Model

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -4,17 +4,38 @@ package com.bridgelabz.employeepayrollapp.model;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
 
-@Data
-public class EmployeePayrollData {
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+//@Entity
+//@Table(name = "employee_payroll")
+public @Data class EmployeePayrollData {
     private int employeeId;
+
+    //@Column(name = "name")
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> department;
 
     public EmployeePayrollData() {}
 
     public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
         this.employeeId = empId;
+        this.updateEmployeePayrollData(empPayrollDTO);
+    }
+
+    public void updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
         this.name = empPayrollDTO.name;
         this.salary = empPayrollDTO.salary;
+        this.gender = empPayrollDTO.gender;
+        this.note = empPayrollDTO.note;
+        this.startDate = empPayrollDTO.startDate;
+        this.profilePic = empPayrollDTO.profilePic;
+        this.department = empPayrollDTO.department;
     }
 }
